c8537fcc1838d523f3a58b05ac9d427e6aaa427f
martiz058 <o.martiz@outlook.com>
2024-06-11 15:02:05 -0700

    remove useNewUrlParser

--- app.js
+++ app.js
@@ -12,10 +12,7 @@ const path = require('path');
 // Connect to MongoDB
 const db_url = process.env.DB_URL || 'mongodb://localhost:27017/unhappyFish';
 
-mongoose.connect(db_url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(db_url)
     .then(() => {
         console.log('Connected to MongoDB - unhappyFish');
     }).catch((error) => {
