57f8711893aed46deb9078a523b20239927de91c
martiz058 <o.martiz@outlook.com>
2024-08-14 20:04:58 -0700

    ejs added to homePage

--- assets/controllers/homeController.js
+++ assets/controllers/homeController.js
@@ -2,41 +2,13 @@
 const { SiteModel, StoryModel } = require('../models/mongoSchema');
 
 module.exports.getHome = async (req, res) => {
-    // Pagination
-    const itemsPerPage = 4;
+    const itemsPerPage = 6;
     const locations = await SiteModel.find({})
-        .limit(itemsPerPage);
+        .limit(6);
     const stories = await StoryModel.find({})
-        .limit(itemsPerPage / 2);
+        .limit(2);
 
-
-    // content
-    const features = [{
-        image: "/data/images/fish-spin.png", title: "Upload Photos",
-        text: "Share your fishing experiences with the community", animation: "spin-image"
-    }, {
-        image: "/data/images/starfish-0.png", title: "Rate Locations",
-        text: "Rate and discover the best fishing spots.", animation: "star"
-    }, {
-        image: "/data/images/fish-net.png", title: "User Stories",
-        text: "Read and share stories from fellow fishers."
-    }, {
-        image: "/data/images/maritime-laws.png", title: "Fishing Manuals",
-        text: "Learn new techniques and improve your skills."
-    }];
-
-    const imageUrls = [
-        "/data/images/fish-0.png",
-        "/data/images/fish-1.png",
-        "/data/images/fish-2.png",
-        "/data/images/fish-3.png",
-        "/data/images/fish-4.png",
-        "/data/images/fish-5.png",
-        "/data/images/fish-6.png",
-        "/data/images/fish-7.png",
-    ];
-
-    res.render('home/home', { locations, stories, features, imageUrls });
+    res.render('home/home', { locations, stories });
 };
 
 module.exports.sendEmail = (req, res) => {
